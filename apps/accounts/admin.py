fa982cb0650a25e70c2f54a9fb386776874396f0
creanorcode <p03k9o0w@students.codeinstitute.net>
2026-02-26 21:07:41 +0100

    Feat: Show CustomerProfile inline on Django User in admin.

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from .models import CustomerProfile
+
+
+class CustomerProfileInline(admin.StackedInline):
+    model = CustomerProfile
+    can_delete = False
+    extra = 0
+
+
+class UserAdmin(DjangoUserAdmin):
+    inlines = [CustomerProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
