d2686cda5d9ee6c73d02deb6e31c19981fba1eab
creanorcode <p03k9o0w@students.codeinstitute.net>
2026-02-27 19:11:29 +0100

    Feat: add robots.txt with sitemap reference and private paths disallowed.

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
 from apps.pages.sitemaps import StaticViewSitemap
+from django.views.generic import TemplateView
 
 sitemaps = {"static": StaticViewSitemap()}
 
@@ -15,4 +16,5 @@ urlpatterns = [
     path("accounts/", include("apps.accounts.urls")),
     path("crm/", include("apps.crm.urls")),
     path("portal/", include("apps.portal.urls")),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
