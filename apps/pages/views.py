4440930426a07c6e6cacb141f30b505150b45eb4
creanorcode <p03k9o0w@students.codeinstitute.net>
2026-02-27 18:08:29 +0100

    Feat: add views for dedicated service pages.

--- apps/pages/views.py
+++ apps/pages/views.py
@@ -1,13 +1,33 @@
 from django.shortcuts import render
 
+
 def home(request):
     return render(request, "pages/home.html")
+
 
 def services(request):
     return render(request, "pages/services.html")
 
+
+def service_web_design(request):
+    return render(request, "pages/services/web_design.html")
+
+
+def service_hosting(request):
+    return render(request, "pages/services/hosting.html")
+
+
+def service_domain(request):
+    return render(request, "pages/services/domain.html")
+
+
+def service_bundle(request):
+    return render(request, "pages/services/bundle.html")
+
+
 def packages(request):
     return render(request, "pages/packages.html")
+
 
 def about(request):
     return render(request, "pages/about.html")
