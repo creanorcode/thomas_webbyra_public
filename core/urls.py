e50d1f686e08361d27956252913740c9cc2a6c4e
creanorcode <p03k9o0w@students.codeinstitute.net>
2026-02-27 19:23:21 +0100

    Correct: corrected core/urls.py 'urlpatterns'.

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
-from apps.pages.sitemaps import StaticViewSitemap
 from django.views.generic import TemplateView
 
-sitemaps = {"static": StaticViewSitemap()}
+from apps.pages.sitemaps import StaticViewSitemap
 
-urlpatterns += [
-    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
-]
+sitemaps = {
+    "static": StaticViewSitemap(),
+}
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -16,5 +15,11 @@ urlpatterns = [
     path("accounts/", include("apps.accounts.urls")),
     path("crm/", include("apps.crm.urls")),
     path("portal/", include("apps.portal.urls")),
-    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+
+    # SEO
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
+    path("robots.txt", TemplateView.as_view(
+        template_name="robots.txt",
+        content_type="text/plain"
+    )),
 ]
