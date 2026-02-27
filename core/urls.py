6ba985197e212c86a86978c7ff506e2dc3a7e9ed
creanorcode <p03k9o0w@students.codeinstitute.net>
2026-02-27 19:06:46 +0100

    Feat: expose sitemap.xml endpoint

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from apps.pages.sitemaps import StaticViewSitemap
+
+sitemaps = {"static": StaticViewSitemap()}
+
+urlpatterns += [
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
+]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
